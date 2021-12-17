98acedca21b3a5df5ba445b251e7b65d17b739fb
seantolbert <84528717+seantolbert@users.noreply.github.com>
2021-12-16 23:18:01 -0500

    reconfigures settings, urls, admin, and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+from django.shortcuts import reverse
 
-# Create your models here.
+CATEGORY = (
+    ("L", "Local Show"),
+    ("R", "Rental"),
+)
+
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY, max_length=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.item_name
+
+    def get_absolute_url(self):
+        return reverse("main_app:product", kwargs={"pk": self.pk})
+
+    def get_add_to_cart_url(self):
+        return reverse("main_app:add-to-cart", kwargs={"pk": self.pk})
+
+    def get_remove_from_cart_url(self):
+        return reverse("main_app:remove-from-cart", kwargs={"pk": self.pk})
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.item_name}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField()
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
